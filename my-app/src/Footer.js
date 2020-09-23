0fcc7a9d9b83f9cb0915ee1f317e90d2abb759ab
Bridgit Arika <bridgitmob@gmail.com>
2020-09-23 17:53:56 -0500

    finished with my child components

---
+++ my-app/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <p>This is where my footer goes...</p>
+    )
+}
+
+export default Footer
---
+++ my-app/src/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent(){
+    return(
+        <div>
+            <h3>Main Content</h3>
+            <p>This is where the main content goes....</p>
+        </div>
+    )
+}
+
+export default MainContent
