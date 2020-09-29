3da1180619d2431c73a71d8b3c6c8cf5c89cd327
Bridgit Arika <bridgitmob@gmail.com>
2020-09-28 22:47:44 -0500

    making a todoitem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Todoitem from './Todoitem'
 
 function App() {
   return (
---
+++ my-app/src/Todoitem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todoitem(){
+    return(
+        <div>
+        <input type="checkbox" />
+     <p>Placeholder text here</p>
+     </div>
+    )
+}
+
+export default Todoitem
